0f941d1333dc3515f1c134d33f572e5ca26068fa
Ikram Hafidz <krumbs8749@gmail.com>
2022-02-28 11:42:00 +0100

    Commit to Github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+import Header from "../src/components/Header"
+import Meme from "../src/components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
